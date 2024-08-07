446c7c39654839739fee782eedba6731da13212a
Masoud Gorgani <alone.3303@gmail.com>
2024-08-07 14:10:03 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// import the express module
+const express = require("express");
+
+const mongoose = require("mongoose");
+//defind the port number the server will listen on
+const PORT = 3000;
+
+// create an instance of express application
+// because it give us the starting point
+const app = express();
+//mongodb string
+const DB = "mongodb+srv://maswd:Mg%40562314@cluster0.xowm6.mongodb.net/";
+app.get("/hello", (req, res) => {
+  // send a response to the client
+  res.send("Hello, World!");
+});
+
+mongoose.connect(DB).then(() => {
+  console.log("mongoose connected");
+});
+//start the server and listen on port
+app.listen(PORT, "0.0.0.0", function () {
+  // log the NUMBER
+  console.log(`Server is running on port ${PORT}`);
+});
